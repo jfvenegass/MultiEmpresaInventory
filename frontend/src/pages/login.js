14291ef0dc2edca00b1dab3be3c67c51af52c297
Carlosdm <148509655+cdavidms@users.noreply.github.com>
2024-12-03 21:15:30 -0500

    login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'; // Importa useEffect
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import apiClient from '../services/apiClient';
+import apiClient from '../services/apiClient'; // Asegúrate de que este archivo esté correctamente configurado
 
 function Login() {
   const [form, setForm] = useState({ email: '', password: '' });
@@ -23,16 +23,16 @@ function Login() {
     try {
       const response = await apiClient.post('/auth/login', form);
       const { token, rol, client_id } = response.data;
-  
+
       console.log('Access Token:', token);
       console.log('User Role:', rol);
       console.log('Client ID:', client_id);
-  
+
       // Guardar en localStorage
       localStorage.setItem('authToken', token);
       localStorage.setItem('userRole', rol);
       localStorage.setItem('client_id', client_id);
-  
+
       // Redirigir
       navigate('/');
     } catch (error) {
@@ -40,12 +40,15 @@ function Login() {
       setError('Credenciales inválidas');
     }
   };
-  
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-4">Iniciar Sesión</h2>
+        {/* Logo */}
+        <div className="flex justify-center mb-6">
+          <img src="/logo192.png" alt="Logo" className="h-16" /> {/* Cambié a usar /logo192.png */}
+        </div>
+        <h2 className="text-2xl font-bold mb-4 text-center">Iniciar Sesión</h2>
         {error && <p className="text-red-500">{error}</p>}
         <input
           type="email"
